fa14753ae8e737ad74ad8e577dde72b417b9eecc
BeYourMarket <hello@beyourmarket.com>
2015-07-08 01:26:43 +0200

    update assemblyinfo + nugetspec

--- src/Plugins/Plugin.Widget.GoogleAnalytics/Properties/AssemblyInfo.cs
+++ src/Plugins/Plugin.Widget.GoogleAnalytics/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Plugin.Widget.GoogleAnalytics")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".net opensource marketplace")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BeYourMarket")]
 [assembly: AssemblyProduct("Plugin.Widget.GoogleAnalytics")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
--- src/Tests/BeYourMarket.Test/Properties/AssemblyInfo.cs
+++ src/Tests/BeYourMarket.Test/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BeYourMarket.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".net opensource marketplace")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BeYourMarket")]
 [assembly: AssemblyProduct("BeYourMarket.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
