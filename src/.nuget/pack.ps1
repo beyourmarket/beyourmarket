dff37c2bb1252ed6f0596e9ab963b902ab5fcd11
BeYourMarket <hello@beyourmarket.com>
2015-07-09 01:49:00 +0200

    fix nuget

--- src/.nuget/pack.ps1
+++ src/.nuget/pack.ps1
@@ -1,3 +1,17 @@
+Param (
+    $parameters = @{},
+    $srcFolder,
+    $projectName,
+    $projectVersion
+)
+
+# update package version in nuspec file
+Write-Output "Updating version in nuspec file"
+$nuspecPath = ".\src\BeYourMarket.nuspec"
+[xml]$xml = Get-Content $nuspecPath
+$xml.package.metadata.version = $projectVersion
+$xml.Save($nuspecPath)
+
 # build NuGet package
 Write-Output "Building NuGet package"
 & .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec
