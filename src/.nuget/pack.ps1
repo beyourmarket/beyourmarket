50aef5f0892380df7b1b94a1bad3ad8cbe9452b4
BeYourMarket <hello@beyourmarket.com>
2015-07-09 01:35:43 +0200

    nuget

--- src/.nuget/pack.ps1
+++ src/.nuget/pack.ps1
@@ -1,3 +1,5 @@
 # build NuGet package
 Write-Output "Building NuGet package"
-.\NuGet.exe pack ..\BeYourMarket.nuspec
+& .\NuGet.exe pack ..\BeYourMarket.nuspec
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
