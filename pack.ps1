db6c5c18ef264307cd09ad7de6602d07a0d5d9f9
BeYourMarket <hello@beyourmarket.com>
2015-07-09 23:18:30 +0200

    build update

--- pack.ps1
+++
@@ -1,3 +0,0 @@
-# build NuGet package
-Write-Output "Building NuGet package"
-& .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec -Version $env:appveyor_build_version
--- src/.nuget/pack.ps1
+++ src/.nuget/pack.ps1
@@ -1,10 +1,10 @@
 # update package version in nuspec file
 Write-Output "Updating version in nuspec file"
-$nuspecPath = "C:\projects\beyourmarket\src\BeYourMarket.nuspec"
+$nuspecPath = "$env:appveyor_build_version\src\BeYourMarket.nuspec"
 [xml]$xml = Get-Content $nuspecPath
 $xml.package.metadata.version = [string]$env:appveyor_build_version
 $xml.Save($nuspecPath)
 
 # build NuGet package
 Write-Output "Building NuGet package"
-& .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec
+& .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec -IncludeReferencedProjects
