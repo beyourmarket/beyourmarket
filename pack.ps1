1df98440162432e78eebe67b486adc77c15a8b8c
BeYourMarket <hello@beyourmarket.com>
2015-07-09 02:08:41 +0200

    update nuget

--- pack.ps1
+++ pack.ps1
@@ -1,10 +1,3 @@
-# update package version in nuspec file
-Write-Output "Updating version in nuspec file"
-$nuspecPath = ".\src\BeYourMarket.nuspec"
-[xml]$xml = Get-Content $nuspecPath
-$xml.package.metadata.version = [string]$env:appveyor_build_version
-$xml.Save($nuspecPath)
-
 # build NuGet package
 Write-Output "Building NuGet package"
-& .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec
+& .\src\.nuget\NuGet.exe pack .\src\BeYourMarket.nuspec -Version $env:appveyor_build_version
