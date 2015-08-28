891bb29c3de5c928616d4f605c5181903fe39a99
Ken Yeung <ken@beyourmarket.com>
2015-08-28 18:22:51 +0200

    add missing file

---
+++ src/BeYourMarket.Model/ModelsPartial/ListingReview.cs
@@ -0,0 +1,27 @@
+﻿using BeYourMarket.Model.Enum;
+using BeYourMarket.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeYourMarket.Model.Models
+{
+    [MetadataType(typeof(ListingReviewMetaData))]
+    public partial class ListingReview
+    {
+        public string RatingClass
+        {
+            get
+            {
+                return "s" + Math.Round(Rating * 2);
+            }
+        }
+    }
+
+    public class ListingReviewMetaData
+    {        
+    }
+}
